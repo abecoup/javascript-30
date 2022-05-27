3fa7fd012c5cf99782bb16f452542afe91dbf96c
abecoup <abe.coup5@gmail.com>
2022-05-27 19:00:11 -0400

    added css and removeTransition function

--- day01-drum-kit/index.js
+++ day01-drum-kit/index.js
@@ -1,4 +1,17 @@
 window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key=${e.key}]`);
-    console.log(audio)
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`);
+
+    if(!audio) return; // no audio to play
+    audio.currentTime = 0; // restart audio when key pressed
+    audio.play();
+    key.classList.add('playing');
 });
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
